7926fb8a07abed2e45aaf1acd32c3e705eaaddb3
yogi samsudn <yogisamsudin@gmail.com>
2021-05-27 16:49:32 +0700

    Update index.js
    dah bisa jalan check imei. masa bosnya langsung turun tangan...

--- src/Pages/CekImei/index.js
+++ src/Pages/CekImei/index.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button,TouchableOpacity,PermissionsAndroid,Platform,Alert, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Button, TouchableOpacity, PermissionsAndroid, Platform, Alert, TextInput } from 'react-native'
 import { _wsJsonConnectionHTTP } from '../../fungsi/function';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 export async function request_READ_PHONE_STATE() {
- 
+
   try {
     const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE ,
+      PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
       {
         'title': 'ReactNativeCode wants to READ_PHONE_STATE',
         'message': 'ReactNativeCode App needs access to your personal data. '
       }
     )
+
+    if (granted == PermissionsAndroid.RESULTS.GRANTED) return true;
+    else {
+      Alert.alert("Permission Not Granted");
+      return false;
+    }
+    /*
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
  
       Alert.alert("Permission Granted.");
@@ -22,50 +32,56 @@ export async function request_READ_PHONE_STATE() {
       Alert.alert("Permission Not Granted");
  
     }
+    */
   } catch (err) {
     console.warn(err)
+    return false;
   }
 }
 export default class CekImei extends React.Component {
 
-  constructor () {
+  constructor() {
     super()
     this.state = {
-      device_IMEI : '',
-      IMEI : '',
+      device_IMEI: '',
+      IMEI: '',
+      status: 'Checking IMEI...'
     }
   }
+
   async componentDidMount() {
- 
-    await request_READ_PHONE_STATE() ;
+    if (await request_READ_PHONE_STATE() == false) return;
+
     const IMEI = require('react-native-imei')
-    let imei="";
+    let imei = "";
+
     var IMEI_2 = IMEI.getImei().then(imeiList => {
-      this.setState({ device_IMEI : imeiList });
-      imei=imeiList;
-      this.state.setState({
-        device_IMEI : '',
-        IMEI : imei,
+      this.setState({
+        device_IMEI: imeiList,
+        IMEI: imeiList.toString()
       });
-      // console.log(this.state.device_IMEI);
+
+      this.getDeviceIMEI();
     });
- 
-  }
-  getDeviceIMEI = () => {
-    _wsJsonConnectionHTTP("check_imei", "imei_no="+this.state.device_IMEI[0], (d) => {
-     //  Alert.alert("info","status="+this.state.device_IMEI);
-      Alert.alert(d.value);
-   });
+
   }
 
-  render () {
+  getDeviceIMEI = () => {
+    _wsJsonConnectionHTTP("check_imei", "imei_no=" + this.state.IMEI, (d) => {
+      if (d.value > 0) this.props.navigation.navigate('LoanCalculator'); else this.setState({ status: 'IMEI tidak terdaftar, hubungi admin...' });
+    });
+  }
+
+  render() {
     return (
+
       <View style={styles.container}>
-        <Text style={{textAlign: 'center' , marginBottom: 20, fontSize: 20}}>{this.state.device_IMEI[0]} </Text>
-        <Text style={{textAlign: 'center' , marginBottom: 20, fontSize: 20}}>{this.state.device_IMEI[1]} </Text>
-        <TextInput  value={this.state.IMEI}>{this.state.IMEI}</TextInput>
-<Button title="Click Here to Get Device IMEI Number" onPress={this.getDeviceIMEI}/>
+        <TextInput>{this.state.IMEI}</TextInput>
+        <Text>{this.state.status}</Text>
+        {/* <Button title="Click Here to Get Device IMEI Number" onPress={() => this.getDeviceIMEI()} /> */}
       </View>
+
+
     )
   }
 }
